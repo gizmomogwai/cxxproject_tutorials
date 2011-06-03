97c1e5fbe7d848bb2708d977a07733fa7867fbe7
Christian Köstlin <christian.koestlin@gmail.com>
2011-06-03 14:26:29 +0200

    Step 6

--- protobuf_project.rb
+++ protobuf_project.rb
@@ -1,3 +1,3 @@
 cxx_configuration do
-  BinaryLibrary.new('protobuf').set_lib_searchpaths(File.join(PROTOBUF_BASE, 'tmp', 'src', 'lib'))
+  BinaryLibrary.new('protobuf').set_lib_searchpaths(File.join(PROTOBUF_BASE, 'tmp', 'src', '.libs')).set_includes(File.join(PROTOBUF_BASE, 'src'))
 end
