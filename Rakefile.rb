bb414e72774a84a329b5285ded990d870d1f1c97
Christian Köstlin <christian.koestlin@gmail.com>
2011-06-03 13:40:13 +0200

    Step 4

--- Rakefile.rb
+++ Rakefile.rb
@@ -2,5 +2,18 @@ require 'cxxproject'
 
 PROTOBUF_VERSION='2.4.1'
 PROTOBUF_BASE="protobuf-#{PROTOBUF_VERSION}"
+PROTOBUF_TMP = File.join(PROTOBUF_BASE, 'tmp')
+PROTOC = File.join(PROTOBUF_TMP, 'src', 'protoc')
+
+GEN_FOLDER = File.join('addressbook', 'gen')
+directory GEN_FOLDER
+['addressbook.pb.cc'].each do |f|
+  desc 'protoc addressbook.proto'
+  generated_filename = File.join(GEN_FOLDER, 'addressbook', f)
+  file generated_filename => [File.join('addressbook', 'addressbook.proto'), GEN_FOLDER, PROTOC] do
+    command = "#{PROTOC} --cpp_out=#{GEN_FOLDER} #{File.join('addressbook', 'addressbook.proto')}"
+    sh command
+  end
+end
 
 CxxProject2Rake.new(FileList['**/*project.rb'], 'build', GCCChain, '.')
