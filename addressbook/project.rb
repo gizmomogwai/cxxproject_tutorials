6d41de524532b862ffcd67f518ca629ac4060f86
Christian Köstlin <christian.koestlin@gmail.com>
2011-06-08 12:46:49 +0200

    Step 2

--- addressbook/project.rb
+++ addressbook/project.rb
@@ -1,5 +1,10 @@
 cxx_configuration do
 
+  source_lib'addressbook_pb_api',
+    :sources => ['../build/protoc/addressbook/addressbook.pb.cc'],
+    :includes => ['../build/protoc'],
+    :dependencies => ['protobuf']
+
   exe 'addressbook_write',
     :sources => ['addressbook_write.cc'],
     :dependencies => ['addressbook_pb_api']
