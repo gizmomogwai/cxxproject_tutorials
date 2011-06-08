b5f1f6d8e514897e0f8bb8ceae99490493934a03
Christian Köstlin <christian.koestlin@gmail.com>
2011-06-08 12:52:12 +0200

    Step 4

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,2 +1,14 @@
 require 'cxxproject'
+
+PROTOC = '/usr/bin/protoc'
+GEN_FOLDER = File.join('build', 'protoc')
+
+directory GEN_FOLDER
+
+desc 'protoc addressbook.proto'
+file File.join(GEN_FOLDER, 'addressbook', 'addressbook.pb.cc') => [File.join('addressbook', 'addressbook.proto'), GEN_FOLDER, PROTOC] do
+  command = "#{PROTOC} --cpp_out=#{GEN_FOLDER} #{File.join('addressbook', 'addressbook.proto')}"
+  sh command
+end
+
 CxxProject2Rake.new(FileList['**/*project.rb'], 'build', GCCChain, '.')
